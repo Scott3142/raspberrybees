80a00e2eead04367b6c8897dc7318ffe68809b52
Scott Morgan <smorgan@bridgend.ac.uk>
2019-04-17 11:22:09 +0100

    Added gitignore and updates to run on boot

--- runstream.sh
+++ runstream.sh
@@ -1,1 +1,5 @@
-raspivid -o - -t 0 -w 1280 -h 720 -fps 40 -b 8000000 -g 40 | ffmpeg -ac 1 -f alsa -ac 1 -i hw:1,0 -f h264 -i pipe:0 -c:v copy -c:a aac -ab 128k -g 40 -f flv -r 30 rtmp://a.rtmp.youtube.com/live2/rvu9-tj9v-cb5p-29xp
+#!/bin/bash
+
+KEY=${1?Error: Must provide YouTube streaming key}
+
+/usr/bin/raspivid -o - -t 0 -w 1280 -h 720 -fps 40 -b 8000000 -g 40 -n | /usr/bin/ffmpeg -ac 1 -f alsa -ac 1 -i hw:1,0 -f h264 -i pipe:0 -c:v copy -c:a aac -ab 128k -g 40 -f flv -r 30 rtmp://a.rtmp.youtube.com/live2/$KEY
